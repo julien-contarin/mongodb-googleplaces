ca01f10196d1c52b86315abcd9426580b8bf6ae6
Julien Contarin <julien.contarin@mongodb.com>
2018-08-29 16:14:02 +0200

    Simplified "updateOne" routine
    This version will insert the full JSON document from Google Places into MongoDB Document as opposed to insert each field individually
    
    You must now simply change the "queryFields" variable to update the list of fields you require for your implementation

--- function_getGoogleInfo.js
+++ function_getGoogleInfo.js
@@ -21,7 +21,7 @@ exports = function(changeEvent) {
         var search_result = EJSON.parse(resp.body.text());
 
         // ACTION REQUIRED BELOW: edit the list of Google Places fields you are interested in. See documentation for full list.
-        // Be careful, since those fields are also listed under the update routine of the MongoDB document below. All fields that are updated by the below Update routine should be present in the below query fields
+        // Be careful, all fields you list below will be in the MongoDB document per the updateOne procedure (this is because the update procedure inserts the full JSON document straight from Google API into MongoDB) 
         queryFields = "formatted_address,geometry,name,place_id,type,vicinity,formatted_phone_number,international_phone_number,opening_hours,website,rating";
         GoogleDetailsURL ="https://maps.googleapis.com/maps/api/place/details/json?placeid="+search_result.candidates[0].place_id+"&fields="+queryFields+"&key="+context.values.get("GooglePlacesAPIKey");
 
@@ -34,24 +34,14 @@ exports = function(changeEvent) {
               var details_result = EJSON.parse(resp.body.text());
 
               // ACTION REQUIRED BELOW: Replace database name and collection name so it matches yours
-              // ACTION REQUIRED BELOW: Make sure all fields listed below as also part of the Google API query (see queryFields variable above)
               // WHAT THIS DOES: Uses Google Places info and pushes it to MongoDB Document. This also reformats Google Places Geo info to make it GeoJSON-ready
+              // All fields added to the queryFields will be pushed to the MongoDB document inside the nested document googlePlaceInfo
               var collection = context.services.get("mongodb-atlas").db("mycompany").collection("mycustomers");
               collection.updateOne(
                 {"_id":fullDocument._id},
                 {$set:{
                   "populatedOn":Date(),
-                  "googlePlaceInfo.name":details_result.result.name,
-                  "googlePlaceInfo.place_id":details_result.result.place_id,
-                  "googlePlaceInfo.geometry":details_result.result.geometry,
-                  "googlePlaceInfo.formatted_address":details_result.result.formatted_address,
-                  "googlePlaceInfo.international_phone_number":details_result.result.international_phone_number,
-                  "googlePlaceInfo.formatted_phone_number":details_result.result.formatted_phone_number,
-                  "googlePlaceInfo.rating":details_result.result.rating,
-                  "googlePlaceInfo.type":details_result.result.type,
-                  "googlePlaceInfo.vicinity":details_result.result.vicinity,
-                  "googlePlaceInfo.website":details_result.result.website,
-                  "googlePlaceInfo.opening_hours":details_result.result.opening_hours,
+                  "googlePlaceInfo":details_result.result,
                   "googlegeoJSONcoordinates.type":"Point"},
                   $push:{ "googlegeoJSONcoordinates.coordinates" : {$each:
                     [details_result.result.geometry.location.lng,
